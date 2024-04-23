ff170c5ea6344ce2d7e0e3bc0529ee5ba5f1f34b
hillview0303 <ujin0105@gmail.com>
2024-04-23 16:10:51 +0900

    챌린지페이지

--- lib/pages/challenge/components/ongoing_challenge_container.dart
+++ lib/pages/challenge/components/ongoing_challenge_container.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../models/challenge.dart';
+import '../ongoing_challenge_detail_page.dart';
 
 class OngoingChallengeContainer extends StatelessWidget {
   final Challenge challenge;
@@ -8,11 +9,15 @@ class OngoingChallengeContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final DateTime now = DateTime.now();
+    final int daysLeft = challenge.closingTime.difference(now).inDays;
+
     return Container(
       margin: EdgeInsets.all(8),
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.7), // 흰색 반투명 배경으로 내부 컨테이너를 강조
+        color: Colors.white.withOpacity(0.7),
         borderRadius: BorderRadius.circular(8),
       ),
       child: Row(
@@ -22,7 +27,7 @@ class OngoingChallengeContainer extends StatelessWidget {
             height: 50,
             padding: EdgeInsets.all(5),
             decoration: BoxDecoration(
-              color: Colors.teal.shade100,  // 아이콘 배경을 밝은 teal로 설정
+              color: Colors.teal.shade100,
               shape: BoxShape.circle,
             ),
             child: ClipOval(
@@ -34,8 +39,10 @@ class OngoingChallengeContainer extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(challenge.name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Text(challenge.challengeName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
                 Text('${challenge.distance} km', style: TextStyle(color: Colors.grey)),
+                Text('마감까지 $daysLeft일 남음', style: TextStyle(color: Colors.red)),
+                Text('이번 도전으로 ${challenge.coin} 코인 획득가능', style: TextStyle(color: Colors.yellow)),
               ],
             ),
           ),
---
+++ lib/pages/challenge/ongoing_challenge_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OngoingChallengeDetailPage extends StatelessWidget {
+  const OngoingChallengeDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("OngoingChallengeDetailPage"));
+  }
+}

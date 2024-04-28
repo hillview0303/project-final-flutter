c503e3710b99e60976cb44afa872f362af604d00
coderyu5523 <coderyu5523@naver.com>
2024-04-28 17:18:53 +0900

    pubspect setting

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey, // context가 없는 곳에서 context를 사용할 수 있는 방법
       debugShowCheckedModeBanner: false,
       theme: theme(),
       home: const WelcomePage(),

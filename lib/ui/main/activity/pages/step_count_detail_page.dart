ab3b3742b4a0b25568245c4264643198f60f52a9
hillview0303 <ujin0105@gmail.com>
2024-05-02 00:38:26 +0900

    걸음 상세페이지

--- lib/ui/main/activity/pages/step_count_detail_page.dart
+++ lib/ui/main/activity/pages/step_count_detail_page.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:project_app/_core/constants/constants.dart';
-import 'package:syncfusion_flutter_gauges/gauges.dart';
+import '../widgets/activity_card_widget.dart';
+import '../widgets/buffer_progress_widget.dart';
+import '../widgets/chart_background_widget.dart';
 
-class StepCountDetailPage extends StatelessWidget {
+class StepCountDetailPage extends StatefulWidget {
+  @override
+  _StepCountDetailPageState createState() => _StepCountDetailPageState();
+}
+
+class _StepCountDetailPageState extends State<StepCountDetailPage> {
+  int currentSteps = 8000;
+  int totalMonthlySteps = 240000;
+  int averageDailySteps = 8000;
+  double averageAchievementRate = 75.0;
+  String maxStepDay = "2024-04-15";
+  int maxSteps = 15000;
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, // 탭의 수
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Daily Steps'),
+          title: Text(
+            '걸음 수',
+            style: TextStyle(color: Colors.white), // 제목 텍스트 색상을 흰색으로 설정
+          ),
           backgroundColor: kAccentColor2,
+          iconTheme: IconThemeData(color: Colors.white), // 아이콘 색상을 흰색으로 설정
           bottom: TabBar(
             tabs: [
               Tab(text: 'Day'),
@@ -18,6 +36,7 @@ class StepCountDetailPage extends StatelessWidget {
               Tab(text: 'Month'),
             ],
             indicatorColor: Colors.white,
+            labelColor: Colors.white,
           ),
         ),
         body: Column(
@@ -31,27 +50,56 @@ class StepCountDetailPage extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
             ),
-            SizedBox(height: 20),
             Expanded(
-              flex: 2, // 차트에 더 많은 공간 할당
-              child: getBufferProgress(), // SfRadialGauge 추가
+              flex: 2,
+              child: BufferProgressWidget(progress: 50),
             ),
-            SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                _buildActivityCard('310 kcal', '칼로리'),
-                _buildActivityCard('7500', '총 걸음수'),
+                ActivityCardWidget(data: '310 kcal', label: '칼로리'),
+                ActivityCardWidget(data: '7500', label: '총 걸음수'),
               ],
             ),
             SizedBox(height: 20),
             Expanded(
-              flex: 3, // 차트에 할당된 공간보다 탭바 뷰에 더 많은 공간 할당
+              flex: 3,
               child: TabBarView(
                 children: <Widget>[
-                  Center(child: Text('Daily Chart')),
-                  Center(child: Text('Weekly Chart')),
-                  Center(child: Text('Monthly Chart')),
+                  ChartBackgroundWidget(
+                    milestones: [
+                      {'step': 5000, 'label': '5000걸음 돌파!'},
+                      {'step': 10000, 'label': '10000걸음 돌파!'},
+                      {'step': 20000, 'label': '20000걸음 돌파!'}
+                    ],
+                    currentSteps: currentSteps,
+                  ),
+                  ChartBackgroundWidget(
+                    milestones: [
+                      {'step': 35000, 'label': '35000걸음 주간 목표 달성!'},
+                      {'step': 50000, 'label': '50000걸음 주간 목표 달성!'}
+                    ],
+                    currentSteps: currentSteps,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: TColor.white,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        _buildDataRow("한달 총 걸음수", "$totalMonthlySteps 걸음"),
+                        Divider(),
+                        _buildDataRow("한달 평균 걸음수", "$averageDailySteps 걸음"),
+                        Divider(),
+                        _buildDataRow("한달 평균 달성률", "$averageAchievementRate%"),
+                        Divider(),
+                        _buildDataRow("가장 많이 걸은 날", "$maxStepDay ($maxSteps 걸음)"),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -61,51 +109,13 @@ class StepCountDetailPage extends StatelessWidget {
     );
   }
 
-  Widget _buildActivityCard(String data, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Text(data, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-        SizedBox(height: 4),
-        Text(label),
+  Widget _buildDataRow(String label, String value) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label, style: TextStyle(fontSize: 16)),
+        Text(value, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
       ],
-    );
-  }
-
-  Widget getBufferProgress() {
-    return Container(
-      height: 250,
-      width: 250,
-      child: SfRadialGauge(
-        axes: <RadialAxis>[
-          RadialAxis(
-            showLabels: false,
-            showTicks: false,
-            startAngle: 180,
-            canScaleToFit: true,
-            endAngle: 360,
-            radiusFactor: 0.8,
-            axisLineStyle: AxisLineStyle(
-              thickness: 0.2,
-              color: const Color.fromARGB(30, 0, 169, 181),
-              thicknessUnit: GaugeSizeUnit.factor,
-              cornerStyle: CornerStyle.bothCurve,
-            ),
-            pointers: <GaugePointer>[
-              RangePointer(
-                value: 50,
-                width: 0.2,
-                sizeUnit: GaugeSizeUnit.factor,
-                color: const Color.fromARGB(120, 0, 169, 181),
-                enableAnimation: true,
-                animationDuration: 1500,
-                animationType: AnimationType.linear,
-                cornerStyle: CornerStyle.bothCurve,
-              ),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }

d5417ac4b16a64dd147ac8da504676d8a7f6b8ca
hillview0303 <ujin0105@gmail.com>
2024-05-07 00:44:04 +0900

    달력 조정

--- dev_lib/calendar_agenda/lib/src/controller.dart
+++ dev_lib/calendar_agenda/lib/src/controller.dart
@@ -1,12 +1,28 @@
 import 'calendar.dart';
 
-// CalendarController
 class CalendarAgendaController {
   CalendarAgendaState? state;
 
   void bindState(CalendarAgendaState state) {
     this.state = state;
   }
+
+  // 이전 달로 이동
+  void previousMonth() {
+    if (state != null && state!.selectedDate != null) {
+      DateTime newDate = DateTime(state!.selectedDate!.year, state!.selectedDate!.month - 1);
+      state!.getDate(newDate);
+    }
+  }
+
+  // 다음 달로 이동
+  void nextMonth() {
+    if (state != null && state!.selectedDate != null) {
+      DateTime newDate = DateTime(state!.selectedDate!.year, state!.selectedDate!.month + 1);
+      state!.getDate(newDate);
+    }
+  }
+
 
   void goToDay(DateTime date) {
     state!.getDate(date);

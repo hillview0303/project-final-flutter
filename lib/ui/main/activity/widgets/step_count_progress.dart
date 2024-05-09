0a79501caa58624ed32aa5568917e6c7f6382f2f
hillview0303 <ujin0105@gmail.com>
2024-05-09 22:52:29 +0900

    활동 만보기 적용

--- lib/ui/main/activity/widgets/step_count_progress.dart
+++ lib/ui/main/activity/widgets/step_count_progress.dart
@@ -2,21 +2,27 @@ import 'package:flutter/material.dart';
 import '../widgets/buffer_progress_widget.dart';
 
 class StepCountProgress extends StatelessWidget {
+  final int currentSteps;
+
+  StepCountProgress({required this.currentSteps});
+
   @override
   Widget build(BuildContext context) {
+    int goalSteps = 10000; // 예시 목표 걸음 수
+    double progressPercentage = currentSteps / goalSteps * 100;
+
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.all(1.0),
+          padding: const EdgeInsets.all(8.0),
           child: Text(
-            '목표의 40%를 달성했어요!',
+            '목표의 ${progressPercentage.toStringAsFixed(1)}%를 달성했어요!',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             textAlign: TextAlign.center,
           ),
         ),
-        BufferProgressWidget(goalSteps: 10000, currentSteps: 5000),
+        BufferProgressWidget(goalSteps: goalSteps, currentSteps: currentSteps),
       ],
     );
   }
 }
-
--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '_common/bottom_navigation_bar.dart';
 import 'main/activity/activity_page.dart';
@@ -8,7 +9,6 @@ import 'main/today/today_page.dart';
 
 class MainPage extends StatefulWidget {
   final int index;
-
   MainPage(this.index);
 
   @override
@@ -17,20 +17,93 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
-
   _MainPageState(this._selectedIndex);
+
+  @override
+  void initState() {
+    super.initState();
+    _requestActivityRecognitionPermission();
+  }
+
+  Future<void> _requestActivityRecognitionPermission() async {
+    var status = await Permission.activityRecognition.status;
+    if (!status.isGranted) {
+      var result = await Permission.activityRecognition.request();
+      _handlePermissionStatus(result);
+    }
+  }
+
+  void _handlePermissionStatus(PermissionStatus status) {
+    if (status.isGranted) {
+      // 권한이 허용된 경우
+      print("Permission granted");
+    } else {
+      if (status.isPermanentlyDenied) {
+        // '다시 묻지 않기' 선택 후 거부
+        _showSettingsDialog();
+      } else {
+        // 일반 거부
+        _showPermissionDialog();
+      }
+    }
+  }
+
+  void _showSettingsDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("권한 필요"),
+        content: Text("이 앱은 활동 인식 권한이 필요합니다. 설정에서 권한을 허용해주세요."),
+        actions: <Widget>[
+          TextButton(
+            child: Text("설정으로 이동"),
+            onPressed: () {
+              Navigator.of(context).pop();
+              openAppSettings();
+            },
+          ),
+          TextButton(
+            child: Text("취소"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showPermissionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("권한 필요"),
+        content: Text("이 앱은 활동 인식 권한이 필요합니다. 권한을 허용해주세요."),
+        actions: <Widget>[
+          TextButton(
+            child: Text("허용"),
+            onPressed: () {
+              Navigator.of(context).pop();
+              _requestActivityRecognitionPermission(); // 권한 요청 다시 시도
+            },
+          ),
+          TextButton(
+            child: Text("취소"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: _buildProfileAppBar(),
       body: IndexedStack(
         index: _selectedIndex,
         children: [
-          TodayPage(), // 오늘의 나
-          ActivityPage(), // 나의 활동
-          ChallengePage(), // 챌린지
-          MyPage() // 마이페이지
+          TodayPage(),
+          ActivityPage(),
+          ChallengePage(),
+          MyPage()
         ],
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
@@ -43,20 +116,4 @@ class _MainPageState extends State<MainPage> {
       ),
     );
   }
-// AppBar _buildProfileAppBar() {
-//   return AppBar(
-//
-//     actions: <Widget>[
-//       IconButton(
-//         icon: Padding(
-//           padding: const EdgeInsets.only(right: 20),
-//           child: Icon(Icons.notifications_outlined,size: 30),
-//
-//         ), // 종 모양 아이콘
-//         onPressed: () {
-//         },
-//       ),
-//     ],
-//   );
-// }
 }
